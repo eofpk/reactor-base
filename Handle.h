d9cb84b6d059f1d4003a4414b076a297fde80514
eofpk <3206014540@qq.com>
2018-03-18 13:22:30 +0000

    create handle.h
    add handle.h file

---
+++ Handle.h
@@ -0,0 +1,8 @@
+#ifndef HANDLE_H
+#define HANDLE_H
+
+/* The type of a handle is system specific - this example 
+uses UNIX I/O handles, which are plain integer values. */
+typedef int Handle;
+
+#endif

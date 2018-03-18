94d1580fc86e2b3bb02896a7c7576f96626ccfb0
eofpk <3206014540@qq.com>
2018-03-18 13:14:14 +0000

    create error.h
    add error.h file

---
+++ Error.h
@@ -0,0 +1,12 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+/**
+* This function defines the error handling strategy.
+* In this sample code, it simply prints the given error string and 
+* terminates the program.
+*/
+void error(const char* reason);
+void debug(const char* reason);
+
+#endif
